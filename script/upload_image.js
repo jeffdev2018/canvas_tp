c2156914513780ca20cd00901534a362478abcb5
Thomas Gentilcore <thomasgentilcore@gmail.com>
2018-08-20 13:18:50 -0400

    upload_image change + menage select_and_drag

--- script/upload_image.js
+++ script/upload_image.js
@@ -1,34 +1,27 @@
 
 "use strict";
 console.log("Js upload file Started");
-const CSS_CLASS_ENTERED = "entered";
-const SVG_NS = "http://www.w3.org/2000/svg";
-const XLINK_NS = "http://www.w3.org/1999/xlink";
+
 document.addEventListener("DOMContentLoaded", function () {
     let svgs = document.getElementsByTagName("svg");
     console.log(svgs);
     console.assert(svgs, "You have no element with Name 'svg'");
 
     for (let s of svgs) {
-        s.addEventListener("dragenter", function (evt) {
 
-            evt.target.classList.add(CSS_CLASS_ENTERED);
-        });
-        s.addEventListener("dragleave", function (evt) {
-
-            evt.target.classList.remove(CSS_CLASS_ENTERED);
-        });
         s.addEventListener("dragover", function (evt) {
 
             evt.preventDefault();
         });
         s.addEventListener("drop", function (evt) {
-            evt.target.classList.remove(CSS_CLASS_ENTERED);
+            // evt.target.classList.remove(CSS_CLASS_ENTERED);
             evt.preventDefault();
+
             let files = evt.dataTransfer.files;
-            console.log(files);
-            let myfiles = files[0];
-            catch_file(evt.target, myfiles);
+
+            for(let f of files){
+                catch_file(s, f);
+            }
 
         });
     }
@@ -41,18 +34,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function catch_file(s, file) {
-        // Vérifier que la target est bien un élément SVG
+
+        // valider que le fichier est bien une image
+        if(file_is_valid(file)){
+
+            // Vérifier que la target est bien un élément SVG
+            console.assert(s.tagName.toLowerCase() === "svg", "Invalid svg element");
+
+            switch (s.id) {
+                case "drop_area":
+                    // Ajout ou remplacement de la propritété CSS "background-image" de l'inline style de l'élément <svg>
+                    if(s.style.backgroundImage === "none"){
+                        s.setAttribute("style", "background-image: url(\"" + URL.createObjectURL(file) + "\");");
+                    } else {
+                        s.style.backgroundImage = "url(\"" + URL.createObjectURL(file) + "\")";
+                    }
+                    break;
+
+                default:
+
+            }
+        }
+    }
+
+    let btn_reset = document.getElementById("effacerBG");
+
+    btn_reset.addEventListener("click", function(evt){
+        raz_drop(document.getElementById("drop_area"));
+
+    });
+
+    function raz_drop(s) {
+        // Vérifier que s est bien un élément SVG
         console.assert(s.tagName.toLowerCase() === "svg", "Invalid svg element");
+
         switch (s.id) {
+
             case "drop_area":
-                let monImg = document.createElementNS(SVG_NS,"image");
-                monImg.setAttribute("x","0");
-                monImg.setAttribute("y","0");
-                monImg.setAttribute("width","100%");
-                monImg.setAttribute("height","700");
-                monImg.setAttribute("preserveAspectRatio","null");
-                monImg.setAttributeNS(XLINK_NS, "xlink:href", URL.createObjectURL(file));
-                drop_area.appendChild(monImg);
+                s.style.backgroundImage = "";
                 break;
 
             default:
